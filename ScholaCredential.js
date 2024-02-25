7207b120efbbccc5ccb3523524c264c1b2e03874
A91y <ayush.agr254@gmail.com>
2024-02-25 06:47:26 +0530

    done

---
+++ ScholaCredential.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // VC type: ScholaCredential
+  // https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld
+  ScholaCredential: (credentialSubject) => ({
+    id: 1,
+    circuitId: "credentialAtomicQuerySigV2",
+    query: {
+      allowedIssuers: ["*"],
+      type: "Schola",
+      context:
+        "https://raw.githubusercontent.com/SupaDevsTeam/PIDSchemas/main/checkkyc.json-ld",
+      // context:
+      //   "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
+      credentialSubject,
+    },
+  }),
+  // See more off-chain examples
+  // https://0xpolygonid.github.io/tutorials/verifier/verification-library/zk-query-language/#equals-operator-1
+};
---
+++ proofRequest.js
@@ -0,0 +1,18 @@
+const { ScholaCredential } = require("./vcHelpers/ScholaCredential");
+
+// design your own customised authentication requirement here using Query Language
+// https://0xpolygonid.github.io/tutorials/verifier/verification-library/zk-query-language/
+
+const humanReadableAuthReason = "Aggregated Marks should be greater than 80%";
+
+const credentialSubject = {
+  Marks: {
+  },
+};
+
+const proofRequest = ScholaCredential(credentialSubject);
+
+module.exports = {
+  humanReadableAuthReason,
+  proofRequest,
+};
